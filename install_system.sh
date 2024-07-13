16e052973310329f5392a00c86c125014da030cb
永恒之蓝 <61956889+Ne-GH@users.noreply.github.com>
2024-07-14 01:39:42 +0800

    添加自定义boot loader name

--- install_system.sh
+++ install_system.sh
@@ -139,7 +139,7 @@ Behind() {
     case $input in
 		1|"")
 			YES | pacman -S grub efibootmgr
-			read boot_loader_name
+			read -p "boot loader name :" boot_loader_name
 			grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=$boot_loader_name
 			grub-mkconfig -o /boot/grub/grub.cfg            
 			;;
